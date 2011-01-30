c91260e0432123133f7afd2727d12c9177e6f0bf
Warren Smith <wsmith@cpan.org>
2011-01-30 23:13:56 +0000

    Import of WSMITH/Email-Folder-Exchange-2.0 from CPAN.
    gitpan-cpan-distribution: Email-Folder-Exchange
    gitpan-cpan-version:      2.0
    gitpan-cpan-path:         WSMITH/Email-Folder-Exchange-2.0.tar.gz
    gitpan-cpan-author:       WSMITH
    gitpan-cpan-maturity:     released

--- lib/Email/Folder/Exchange.pm
+++ lib/Email/Folder/Exchange.pm
@@ -1,240 +1,55 @@
 package Email::Folder::Exchange;
-use strict;
-
-# vim: ft=perl fdm=marker ts=4 sw=4
-
-our $VERSION = '1.10';
-
 use base qw(Email::Folder);
+
+use strict;
+use warnings;
+
+our $VERSION = '2.0';
+
+use Email::Folder;
+use Email::Folder::Exchange::WebDAV;
+use Email::Folder::Exchange::EWS;
+use Data::Dumper;
+
 use URI;
-use URI::Escape;
 use LWP::UserAgent;
 
-use Carp qw(carp croak);
+sub new {
+  my ($self, $class, $url, $username, $password) = ({}, @_);
+	bless $self, $class;
 
-sub _ua { # {{{
-    my ($self, $ua) = @_;
-    $self->{_ua} = $ua if @_ == 2;
-    return $self->{_ua};
-} # }}}
+	# try EWS first
+	my $folder;
+	eval {
+		$folder = Email::Folder::Exchange::EWS->new($url, $username, $password);
+	};
+	if($@ =~ /Not Found/) {
+	  # try WebDAV second
+    $folder = Email::Folder::Exchange::WebDAV->new($url, $username, $password);
+	}
+	# re-raise
+	die $@ if $@;
 
-sub uri { # {{{
-    my ($self, $uri) = @_;
-    $self->{uri} = $uri if @_ == 2;
-    return $self->{uri};
-} # }}}
-
-sub _login { # {{{
-    my ($self, $uri, $username, $password) = @_;
-    my $scheme = $uri->scheme;
-    my $host = $uri->host;
-    my $ua = $self->_ua;
-
-    # login using FBA (forms-based authentication)
-    my $auth_uri = $uri->clone;
-    $auth_uri->path('exchweb/bin/auth/owaauth.dll');
-    my $login_req = HTTP::Request->new(
-        POST => $auth_uri->as_string,
-    );
-    $login_req->content_type('application/x-www-form-urlencoded');
-    $login_req->content(
-        'destination=' . uri_escape($uri->as_string) . 
-        '&username=' . uri_escape($username) . 
-        '&password=' . uri_escape($password)
-    );
-
-    my $login_res = $ua->request($login_req);
-    croak $login_res->message if $login_res->code >= 400 and $login_res->code < 500;
-
-    return 1;
-} # }}}
-
-sub new { # {{{
-    my ($self, $class, $url, $username, $password) = ({}, @_);
-    bless $self, $class;
-
-    croak "URI required" unless $url;
-
-    # create user agent
-    my $ua = LWP::UserAgent->new( keep_alive => 1, cookie_jar => {} );
-    $self->_ua($ua);
-
-    # create uri object
-    my $uri = URI->new($url);
-    $self->uri($uri);
-
-    # get credentials from url if specified
-    my $credentials = $uri->userinfo;
-    $uri->userinfo(undef);
-
-    if($credentials && !($username || $password)) {
-        ($username, $password) = split(/:/, uri_unescape($credentials), 2);
-    }
-    croak "Credentials required" unless $username;
-
-    $self->_login($uri, $username, $password);
-    
-    return $self;
-} # }}}
-
-sub _message_urls { # {{{
-    my ($self) = @_;
-    return $self->{_message_urls} if $self->{_message_urls};
-
-    my $req = HTTP::Request->new(
-        SEARCH => $self->uri->as_string,
-    );
-    $req->content_type('text/xml');
-    $req->header(Depth => 1);
-
-
-    my $folder_path = $self->uri->path;
-    $req->content(qq{
-        <?xml version='1.0' ?>
-        <a:searchrequest xmlns:a='DAV:'><a:sql>
-        SELECT "DAV:ishidden"
-          FROM scope('shallow traversal of "$folder_path"')
-         WHERE "DAV:ishidden"=False AND "DAV:isfolder"=False
-        </a:sql></a:searchrequest>
-    });
-
-    my $ua = $self->_ua;
-
-    my @message_urls;
-    my $buf = "";
-
-    my $res = $ua->request($req, sub {
-        my $chunk = shift;
-        $buf .= $chunk;
-
-        while($buf =~ m#<a:href>(.*?)</a:href>#g) {
-            push @message_urls, $1;
-
-        }
-        $buf = substr($buf, (pos $buf || 0));
-    });
-    croak $res->message unless $res->code >= 200 and $res->code < 300;
-
-    $self->{_message_urls} = \@message_urls;
-
-    return $self->{_message_urls};
-} # }}}
-
-sub messages { # {{{
-    my $self = shift;
-
-    my @messages;
-    while(my $message = $self->next_message) {
-        push @messages, $message;
-    }
-
-    return @messages;
-} # }}}
- 
-sub next_message { # {{{
-    my $self = shift;
-    my $message_url = shift @{ $self->_message_urls };
-    return undef unless defined $message_url;
-
-    my $req = HTTP::Request->new( GET => $message_url );
-    $req->header(Translate => 'f');
-    my $res = $self->_ua->request($req);
-    croak $res->message unless $res->code >= 200 and $res->code < 300;
-
-    return $self->bless_message($res->content);
-} # }}}
-
-sub _folder_urls { # {{{
-    my ($self) = @_;
-    return $self->{_folder_urls} if $self->{_folder_urls};
-
-    my $req = HTTP::Request->new(
-        SEARCH => $self->uri->as_string,
-    );
-    $req->content_type('text/xml');
-    $req->header(Depth => 1);
-
-    my $folder_path = $self->uri->path;
-    $req->content(qq{
-        <?xml version='1.0' ?>
-        <a:searchrequest xmlns:a='DAV:'><a:sql>
-        SELECT "DAV:ishidden"
-          FROM scope('shallow traversal of "$folder_path"')
-         WHERE "DAV:ishidden"=False AND "DAV:isfolder"=True
-        </a:sql></a:searchrequest>
-    });
-
-    my $ua = $self->_ua;
-
-    my @folder_urls;
-    my $buf = "";
-
-    my $res = $ua->request($req, sub {
-        my $chunk = shift;
-        $buf .= $chunk;
-
-        while($buf =~ m#<a:href>(.*?)</a:href>#g) {
-            push @folder_urls, $1;
-
-        }
-
-        $buf = substr($buf, (pos $buf || 0));
-    });
-    croak $res->message unless $res->code >= 200 and $res->code < 300;
-
-    $self->{_folder_urls} = \@folder_urls;
-
-    return $self->{_folder_urls};
-} # }}}
-
-sub folders { # {{{
-    my $self = shift;
-
-    my @folders;
-    while(my $folder = $self->next_folder) {
-        push @folders, $folder;
-    }
-
-    return @folders;
-} # }}}
-
-sub next_folder { # {{{
-    my $self = shift;
-
-    my $folder_url = shift @{ $self->_folder_urls };
-    return unless defined $folder_url;
-
-    my $folder = $self->clone;
-    $folder->uri(URI->new($folder_url));
-
-    return $folder;
-} # }}}
-
-sub clone { # {{{
-    my $self = shift;
-
-    my $clone = bless {
-        uri => $self->uri->clone,
-        _ua => $self->_ua->clone,
-    }, ref $self;
-
-    # copy cookie jar
-    $clone->_ua->{cookie_jar} = $self->_ua->{cookie_jar};
-
-    return $clone;
-} # }}}
+	return $folder;
+}
 
 1;
+
 __END__
+
 =head1 NAME
 
-Email::Folder::Exchange - Email::Folder access to exchange folders via WebDAV
+Email::Folder::Exchange - Access your Microsoft Exchange 2000/2003/2007/2010 email from perl
 
 =head1 SYNOPSIS
 
   use Email::Folder::Exchange;
 
+	# Access Exchange 2000/2003 via WebDAV
   my $folder = Email::Folder::Exchange->new('http://owa.myorg.com/user/Inbox', 'user', 'password');
+
+	# Access Exchange 2007/2010 via Exchange Web Services
+  my $folder = Email::Folder::Exchange->new('http://owa.myorg.com', 'user', 'password');
 
   for my $message ($folder->messages) {
     print "subject: " . $subject->header('Subject');
@@ -252,8 +67,20 @@ Email::Folder::Exchange - Email::Folder access to exchange folders via WebDAV
 Add access to Microsoft Exchange to L<Email::Folder>. Contains API enhancements
 to allow folder browsing.
 
-Utilizes FBA (forms-based authentication) to login. Therefore, OWA (Outlook Web
-Access) must be installed and enabled on target server.
+Email::Folder::Exchange is a wrapper around two modules [included]:
+
+=over
+
+=item L<Email::Folder::Exchange::WebDAV> - Access Exchange 2000/2003 via WebDAV
+
+=item L<Email::Folder::Exchange::EWS> - Access Exchange 2007/2010 via EWS
+
+=back
+
+Each module has its own extensions to the Email:Folder protocol.
+
+First, the module tries to connect via EWS. If the server reports a 404 error,
+the module attempts to fallback to WebDAV.
 
 =head2 new($url, [$username, $password])
 
@@ -261,11 +88,9 @@ Create Email::Folder::Exchange object and login to OWA site.
 
 =over
 
-=item url
+=item url 
 
-URL of the target folder, usually in the form of server/user/Inbox. May contain
-authentication information, I.E.
-'http://domain\user:password@owa.myorg.com/user/Inbox'.
+URL of the target folder.
 
 =item username
 
@@ -302,9 +127,20 @@ as iterator. Returns undef at end of list.
 
 Return L<URI> locator object for current folder.
 
+=head1 CAVEATS
+
+  Can't locate object method "new" via package "LWP::Protocol::https::Socket"
+
+Install the Crypt::SSLeay module in order to support SSL URLs
+
+ The server cannot service this request right now. Try again later.
+
+This error indicates the mailbox is has too many messages or the specified
+message is too large to retrieve via web services.
+
 =head1 SEE ALSO
 
-L<Email::Folder>, L<URI>, L<Email::Simple>
+L<Email::Folder>, L<URI>, L<Email::Simple>, L<Crypt::SSLeay>
 
 =head1 AUTHOR
 
@@ -312,7 +148,7 @@ Warren Smith <lt>wsmith@cpan.org<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2007 by Warren Smith
+Copyright (C) 2011 by Warren Smith
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.4 or,
--- t/99-live_tests.t
+++ t/99-live_tests.t
@@ -4,43 +4,52 @@ use strict;
 
 # run this test?
 use File::Basename;
+use open IN => ':crlf'; # auto-handle line endings
+
 require Test::More;
 
 my $test_bn = basename($0);
 $test_bn =~ /^\d+-(.*)\.t/;
 my $test_name = $1;
 
-open my $config_fh, "<", "test.config";
+my %config;
+# default configuration
+$config{url} ||= 'http://site.com/exchange/username/Inbox';
+$config{username} ||= 'DOMAIN\\username';
+
+open my $config_fh, "<", "test.config" or die "Could not open test configuration!";
 while(<$config_fh>) {
-    if(/^$test_name\s+(.*)/) {
-        if(!$1) {
-            # dummy test to keep the harness happy
-            import Test::More tests => 1;
-            ok(1);
-            exit;
-        }
-        
-    }
+  chomp;
+  my ($key, $value) = split /\s+/, $_, 2;
+	$config{$key} = $value;
 }
 close $config_fh;
 
-# now the real tests begin
+# create dummy test to keep the harness happy if we're not running this test
+if(!$config{live_tests}) {
+	import Test::More tests => 1;
+	ok(1);
+	exit;
+}
 
+# now the real tests begin
 use Email::Folder::Exchange;
 use UNIVERSAL qw(isa);
 
-import Test::More tests => 8;
+import Test::More tests => 6;
 
 use_ok('Term::ReadKey');
 
 print STDERR "\n";
-print STDERR "URL to test [format: http://site.com/exchange/username/Inbox]: ";
+print STDERR "URL to test [$config{url}]: ";
 my $url = ReadLine(0);
 chomp $url;
+$config{url} = $url if $url;
 
-print STDERR "Username to authenticate with [format: DOMAIN\\username]: ";
+print STDERR "Username to authenticate with [$config{username}]: ";
 my $username = ReadLine(0);
 chomp $username;
+$config{username} = $username if $username;
 
 print STDERR "Password to authenticate with [will not echo]: ";
 ReadMode('noecho');
@@ -48,31 +57,21 @@ my $password = ReadLine(0);
 chomp $password;
 ReadMode('normal');
 
-ok(my $f = Email::Folder::Exchange->new($url, $username, $password), 'login');
+# save config
+open $config_fh, ">", "test.config";
+while(my($key, $value) = each(%config)) {
+	print "$key $value \n";
+  print $config_fh "$key $value\n";
+}
+
+ok(my $f = Email::Folder::Exchange->new($config{url}, $config{username}, $password), 'login');
+
+ok(my $f2 = $f->next_folder, 'next_folder');
+
+ok(isa($f2, 'Email::Folder::Exchange::EWS') || isa($f2, 'Email::Folder::Exchange::WebDAV'), 'folder_type');
 
 ok(my $m = $f->next_message, 'next_message');
 
 ok(isa($m, 'Email::Simple'), 'message_type');
 
-ok(my $f2 = $f->next_folder, 'next_folder');
-
-ok(isa($f2, 'Email::Folder::Exchange'), 'folder_type');
-
-ok(my $m2 = $f->next_message, 'folder_next_message');
-
-ok(isa($m2, 'Email::Simple'), 'message_type');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+1;
